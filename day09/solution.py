71420b25663d314a875e05f91d92a8cfa1c70867
Jop Verbeek <jpa.verbeek@gmail.com>
2025-12-09 22:44:41 +0100

    x

--- day09/solution.py
+++ day09/solution.py
@@ -1,7 +1,6 @@
 with open('input.txt') as f:
     lines = [list(map(int, line.split(','))) for line in f.read().splitlines()]
 
-
 perimeter = set()
 areas = []
 
@@ -9,22 +8,14 @@ for i, (ca, ra) in enumerate(lines):
     for j, (cb, rb) in enumerate(lines):
         if i > j:
             area = (abs(ca - cb) + 1) * (abs(ra - rb) + 1)
-            areas.append(((ca, ra), (cb, rb), area))
+            areas.append((((ca, ra), (cb, rb)), area))
         if ca == cb:
             line = {(ca, num_row) for num_row in range(min(ra, rb), max(ra, rb) + 1)}
         if ra == rb:
             line = {(num_col, ra) for num_col in range(min(ca, cb), max(ca, cb) + 1)}
         perimeter |= line
 
-areas = []
-for i, (ca, ra) in enumerate(lines):
-    for j, (cb, rb) in enumerate(lines):
-        if i > j:
-            area = (abs(ca - cb) + 1) * (abs(ra - rb) + 1)
-            areas.append((((ca, ra), (cb, rb)), area))
-
 areas = sorted(areas, key=lambda x: x[1], reverse=True)
-
 
 for area in areas:
     found = True
@@ -38,5 +29,3 @@ for area in areas:
     if found:
         print(area)
         break
-
-
